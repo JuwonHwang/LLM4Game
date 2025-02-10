732f312cc7ad5ef8b170e4c26916b88550d2c6f8
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-10 17:55:32 +0900

    add overriding status

--- pyautobattle/src/status.py
+++ pyautobattle/src/status.py
@@ -1,17 +1,72 @@
 from .base import Base
 
 class Status(Base):
-    def __init__(self):
-        self.hp = 0
-        self.mp = 0
-        self.attack = 0
-        self.defense = 0
-        self.attackSpeed = 0
-        self.specialAttack = 0
-        self.specialDefense = 0
-        self.criticalRate = 0
-        self.criticalDamage = 0
-        self.range = 0
+    def __init__(
+            self, 
+            hp,
+            mp, 
+            attack, 
+            defense,
+            attackSpeed, 
+            specialAttack, 
+            specialDefense, 
+            criticalRate, 
+            criticalDamage, 
+            attackRange
+        ):
+        self.hp = float(hp)
+        self.mp = float(mp)
+        self.attack = float(attack)
+        self.defense = float(defense)
+        self.attackSpeed = float(attackSpeed)
+        self.specialAttack = float(specialAttack)
+        self.specialDefense = float(specialDefense)
+        self.criticalRate = float(criticalRate)
+        self.criticalDamage = float(criticalDamage)
+        self.attackRange = int(attackRange)
     
     def observe(self):
-        pass
+        return {
+            "hp": self.hp,
+            "mp": self.mp,
+            "attack": self.attack,
+            "defense": self.defense,
+            "attackSpeed": self.attackSpeed,
+            "specialAttack": self.specialAttack,
+            "specialDefense": self.specialDefense,
+            "criticalRate": self.criticalRate,
+            "criticalDamage": self.criticalDamage,
+            "attackRange": self.attackRange,
+        }
+    
+    def __add__(self, other):
+        if isinstance(other, Status):
+            return Status(
+                    self.hp + other.hp,
+                    self.mp + other.mp,
+                    self.attack + other.attack,
+                    self.defense + other.defense,
+                    self.attackSpeed + other.attackSpeed,
+                    self.specialAttack + other.specialAttack,
+                    self.specialDefense + other.specialDefense,
+                    self.criticalRate + other.criticalRate,
+                    self.criticalDamage + other.criticalDamage,
+                    self.attackRange + other.attackRange
+                )
+        return NotImplemented
+    
+    def __sub__(self, other):
+        if isinstance(other, Status):
+            return Status(
+                    self.hp - other.hp,
+                    self.mp - other.mp,
+                    self.attack - other.attack,
+                    self.defense - other.defense,
+                    self.attackSpeed - other.attackSpeed,
+                    self.specialAttack - other.specialAttack,
+                    self.specialDefense - other.specialDefense,
+                    self.criticalRate - other.criticalRate,
+                    self.criticalDamage - other.criticalDamage,
+                    self.attackRange - other.attackRange
+                )
+        return NotImplemented
