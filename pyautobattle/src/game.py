ae3365c37a84ca3b726c8fb2e7d7274721f0e21e
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-10 17:57:18 +0900

    add unit dict and list

--- pyautobattle/src/game.py
+++ pyautobattle/src/game.py
@@ -1,11 +1,15 @@
 from .base import Base
-
+from .unit import Unit
 class AutoBattlerGame(Base):
     def __init__(self, players, seed=0):
         self.seed = seed
         self.players = players
         self.round = 0
+        self.units = []
+        self.unit_dict : dict[str, Unit] = dict()
+        self.unit_counts = [0, 22, 20, 17, 10, 9]
         self.available_units = [
+            {},
             {},
             {},
             {},
@@ -18,4 +22,5 @@ class AutoBattlerGame(Base):
             "seed": self.seed,
             "players": [p.observe() for p in self.players],
             "round": self.round,
+            "available_units": self.available_units
         }
