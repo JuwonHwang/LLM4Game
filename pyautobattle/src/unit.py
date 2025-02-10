fb0e6ea1ecd2db0732ff1d309fdabd523e629619
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-10 17:56:00 +0900

    add unit observe function

--- pyautobattle/src/unit.py
+++ pyautobattle/src/unit.py
@@ -1,13 +1,23 @@
 from .active import Active
 from .status import Status
 
-class Unit(Active, Status):
-    def __init__(self, name: str, cost: int, status: Status, synergy: list[str]):
+class Unit(Active):
+    def __init__(self, name: str, cost: int, level: int, status: Status, synergy: list[str]):
         super().__init__()
-        self.synergy = []
+        self.synergy = synergy
         self.cost = cost
         self.name = name
-        
+        self.level = level
+        self.status = status
+        self.additional_status = Status(0,0,0,0,0,0,0,0,0,0)
+        self.items = []
         
     def observe(self):
-        pass
+        return {
+            "name": self.name,
+            "cost": self.cost,
+            "level": self.level,
+            "synergy": self.synergy,
+            "item": [item.observe() for item in self.items],
+            "status": self.status.observe()
+        }
