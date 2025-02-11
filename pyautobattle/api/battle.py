44b2a1e53fb1ecdb6d44680f9658f544597dac82
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-11 17:38:22 +0900

    add combat

---
+++ pyautobattle/api/battle.py
@@ -0,0 +1,18 @@
+from src import *
+import random
+import numpy as np
+import csv
+import json
+
+def combat(player1: Player, player2: Player):
+    field1 = player1.field.get_combat_mode()
+    field2 = player2.field.get_combat_mode()
+    
+    # Combat phase
+    timer = 3000
+    while timer > 0:
+        pass
+    # Extended combat phase
+    timer = 1500
+    while timer > 0:
+        pass
