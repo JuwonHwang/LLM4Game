c947cab8ad35225a00d3e03909d5fdaf8a9725d2
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-11 17:38:02 +0900

    fix shop init

--- pyautobattle/src/shop.py
+++ pyautobattle/src/shop.py
@@ -1,9 +1,10 @@
 from .base import Base
+from .unit import Unit
 
 class Shop(Base):
     def __init__(self):
         self.max_units = 5
-        self.units = []
+        self.units: list[Unit] = [None] * self.max_units
         
     def observe(self):
         return self.units
