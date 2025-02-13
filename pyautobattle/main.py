d76d0ee9abd8984607e74a9817f04090d2292184
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-13 16:34:23 +0900

    Imploy pygame for visualization

--- pyautobattle/main.py
+++ pyautobattle/main.py
@@ -2,12 +2,13 @@ from src import *
 from api.core import *
 from api.battle import *
 import json
+import sys
+import pygame
+import colorama
+from colorama import Fore, Back, Style
 
-def print_obs(obs: dict):
-    print(json.dumps(obs, indent=4, sort_keys=True))
-
-def wrap(obj: Base):
-    print_obs(obj.observe())
+def renderer(state):
+    print("(f) Buy EXP ($4) | (d) Reroll ($2)")
 
 def main():
     player_num = 8
@@ -20,9 +21,26 @@ def main():
         players[i].gold = 100
         purchase_unit(game, players[i], 0)
         bench_to_field(players[i], 0)
-    result = combat(players[0], players[1])
-    print(result)
-    
+    running = True
+    while running:
+        # Event Handler
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    print(Fore.YELLOW + Back.WHITE + "- Reroll -" + Fore.RESET + Back.RESET)
+                elif event.key == pygame.K_f:
+                    print(Fore.BLUE + Back.WHITE + "- Buy EXP -"  + Fore.RESET + Back.RESET)
+        # Mechanism
+
+
 if __name__ == "__main__":
+    colorama.init()
+    pygame.init()
+    screen_width = 640
+    screen_height = 480
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("pyTFT")
     main()
-    
+    pygame.quit()
