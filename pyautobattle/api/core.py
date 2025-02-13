cbc265c6b83bf9aec368c8036037cf6a6d13bab1
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-13 16:33:12 +0900

    move wrap to core

--- pyautobattle/api/core.py
+++ pyautobattle/api/core.py
@@ -2,6 +2,13 @@ from src import *
 import random
 import numpy as np
 import csv
+import json
+
+def print_obs(obs: dict):
+    print(json.dumps(obs, indent=4, sort_keys=True))
+
+def wrap(obj: Base):
+    print_obs(obj.observe())
 
 def logger(t: str):
     print(t)
