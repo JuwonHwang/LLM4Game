2780808365d3ea8ad35aff3e7c7e3eb9036cdf58
JuwonHwang <juwon0018@postech.ac.kr>
2025-02-14 14:13:59 +0900

    fix floating point

--- pyautobattle/src/status.py
+++ pyautobattle/src/status.py
@@ -15,15 +15,15 @@ class Status(Base):
             criticalDamage, 
             attackRange
         ):
-        self.hp = float(hp)
-        self.mp = float(mp)
-        self.attack = float(attack)
-        self.defense = float(defense)
-        self.attackSpeed = float(attackSpeed)
-        self.specialAttack = float(specialAttack)
-        self.specialDefense = float(specialDefense)
-        self.criticalRate = float(criticalRate)
-        self.criticalDamage = float(criticalDamage)
+        self.hp = float(hp, 2)
+        self.mp = float(mp, 2)
+        self.attack = float(attack, 2)
+        self.defense = float(defense, 2)
+        self.attackSpeed = float(attackSpeed, 2)
+        self.specialAttack = float(specialAttack, 2)
+        self.specialDefense = float(specialDefense, 2)
+        self.criticalRate = float(criticalRate, 2)
+        self.criticalDamage = float(criticalDamage, 2)
         self.attackRange = int(attackRange)
     
     def observe(self):
